0c668102f3dc70241a94d54a8bb40afb630662ef
Alejandro <alixxantada@gmail.com>
2022-05-31 10:38:29 +0200

    organizando header y footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { TitleComponent } from './title/title.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    TitleComponent
+    TitleComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    TitleComponent
+    TitleComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
