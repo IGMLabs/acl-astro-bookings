9996e8201dce3a868526f29f798d96498854889a
Alejandro <alixxantada@gmail.com>
2022-05-31 11:55:23 +0200

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'',
+    component: HomePage
+  },
+  // No haria falta hacer la carga perezosa de forma manual, hay comando ( ng  )
+  // {
+  //   path:'about',
+  //   loadChildren: () => import('./about/about.module').then( (m) => m.AboutModule),
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
