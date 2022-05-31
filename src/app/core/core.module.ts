dd8f0d7d7e0dcb60cd734988db167309b931e23f
Alejandro <alixxantada@gmail.com>
2022-05-31 10:22:59 +0200

    title component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { TitleComponent } from './title/title.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    TitleComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    TitleComponent
   ]
 })
 export class CoreModule { }
