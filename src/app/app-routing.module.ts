ac6d29617e2346bbb9dc09e86903fea1bd5aa6ed
Alejandro <alixxantada@gmail.com>
2022-05-31 12:27:24 +0200

    cotnat routing ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,9 @@ const routes: Routes = [
      path:'about',
      loadChildren: () => import('./about/about.module').then( (m) => m.AboutModule),
    },
+  { path: 'contact',
+   loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)
+  },
 ];
 
 @NgModule({
