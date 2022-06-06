6c04e847829973daf63bc7099b6e15ae232360e7
Alejandro <alixxantada@gmail.com>
2022-06-06 12:50:36 +0200

    new agency form ok

--- src/app/agencies/new-agency/new-agency.module.ts
+++ src/app/agencies/new-agency/new-agency.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { NewAgencyRoutingModule } from './new-agency-routing.module';
 import { NewAgencyComponent } from './new-agency.component';
+import { NewAgencyPage } from './new-agency.page';
+import { NewAgencyForm } from './new-agency.form';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    NewAgencyComponent
+    NewAgencyComponent,
+    NewAgencyPage,
+    NewAgencyForm
   ],
   imports: [
     CommonModule,
-    NewAgencyRoutingModule
+    NewAgencyRoutingModule,
+    SharedModule
   ]
 })
 export class NewAgencyModule { }
