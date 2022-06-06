d39549555d2401abe9e6be787a12f0affb499bca
Alejandro <alixxantada@gmail.com>
2022-06-06 11:02:51 +0200

    form contact 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 interface Contact {
   name: string;
@@ -20,16 +20,54 @@ export class ContactComponent implements OnInit {
   constructor( formBuilder:FormBuilder ) {
     // Inicializamos el formulario de contacto
     this.form = formBuilder.group({
-      name: ['', Validators.required],
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      message: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
     });
   }
 
-  ngOnInit(): void {
+  public hasErrors( controlName:string ):boolean {
+    const control = this.getControl('name');
+    if (!control) return false;
+    return control.invalid;
   }
+
+
+  public mustShowMessage(controlName: string): boolean {
+    const control = this.getControl(controlName);
+    if (!control) return false;
+    return control.touched && control.invalid;
+  }
+
+  public getErrorMessage(controlName:string) : string {
+    const control = this.getControl(controlName);
+    if (!control) return '';
+    if(!control.errors) return '';
+    const errors = control.errors;
+    let errorMessage = '';
+    // indicamos si ha pasado la validacion de required
+    errorMessage +=errors['required'] ? '🎀 Campo Requerido' : '';
+    errorMessage +=errors['email'] ? '🎀 Campo Requerido' : '';
+    errorMessage +=errors['minlength'] ? `🎀 El campo debe tener almenos ${errors['minlength'].requiredMinLength} caracteres` : '';
+    errorMessage +=errors['maxlength'] ? `🎀 El campo debe tener maximo ${errors['maxlength'].requiredMaxLength} caracteres` : '';
+    return errorMessage;
+  }
+
+
+  public getControl(controlName: string): AbstractControl | null {
+    return this.form.get(controlName);
+  }
+
+
 
   public onSave(){
       const contact = this.form.value;
       console.warn('Send contact message', contact);
+  }
+
+
+  ngOnInit(): void {
+
   }
 
 }
