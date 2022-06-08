094e881ab19e7a566b8b2269091cad6b2be72dbe
Alejandro <alixxantada@gmail.com>
2022-06-08 11:48:32 +0200

    trip api ok

--- src/app/shared/components/trips/trips.list.ts
+++ src/app/shared/components/trips/trips.list.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Trip } from 'src/app/core/api/trip.interface';
+import { FormMessagesService } from 'src/app/core/services/forms/form-messages.service';
+import { TripsApi } from '../../../core/api/trips.api';
 
 @Component({
   selector: 'app-trips-list',
@@ -7,83 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TripsList implements OnInit {
 
+  public trips: Trip[];
   public reloading = false;
 
 
   public reload(lista: string) {
-    this.reloading=true;
-    console.log('Reloading.....'+lista);
+    this.reloading = true;
+    console.log('Reloading.....' + lista);
   }
 
-  trips = [
-    {
-      id: 'space-y-moon-1',
-      agencyId: 'space-y',
-      agencyTripCode: 'moon',
-      destination: 'The Moon',
-      places: 14,
-      startDate: '2023-01-01',
-      endDate: '2023-02-01',
-      flightPrice: 1200000,
-      stayingNightPrice: 10000,
-      kind: 'WithStay',
-      status: 'Confirmed',
-      extraLuggagePricePerKilo: 2000,
-      premiumFoodPrice: 0,
-    },
-    {
-      id: 'space-y-mars-2',
-      agencyId: 'space-y',
-      agencyTripCode: 'mars',
-      destination: 'Mars',
-      places: 8,
-      startDate: '2024-01-01',
-      endDate: '2024-05-01',
-      flightPrice: 8400000,
-      stayingNightPrice: 10000,
-      kind: 'WithStay',
-      status: 'Waiting',
-      extraLuggagePricePerKilo: 50000,
-      premiumFoodPrice: 0,
-    },
-    {
-      id: 'green-origin-low-orbit-3',
-      agencyId: 'green-origin',
-      agencyTripCode: 'low-orbit',
-      destination: 'Low Orbit',
-      places: 0,
-      startDate: '2022-04-01',
-      endDate: '2022-04-01',
-      flightPrice: 320000,
-      stayingNightPrice: 0,
-      kind: 'TripOnly',
-      status: 'Waiting',
-      extraLuggagePricePerKilo: 0,
-      premiumFoodPrice: 1000,
-    },
-    {
-      id: 'green-origin-iss-4',
-      agencyId: 'green-origin',
-      agencyTripCode: 'iss',
-      destination: 'ISS',
-      places: 6,
-      startDate: '2022-06-01',
-      endDate: '2022-06-01',
-      flightPrice: 800000,
-      stayingNightPrice: 0,
-      kind: 'TripOnly',
-      status: 'Waiting',
-      extraLuggagePricePerKilo: 0,
-      premiumFoodPrice: 1000,
-    },
-  ];
+
+  constructor(tripsApi: TripsApi,
+    fms: FormMessagesService,
+  ) {
+    // super(fms);
+    this.trips = tripsApi.getAll();
+
+  }
+
+
 
   public getTripsLength(){
     return this.trips.length;
     }
-
-
-  constructor() { }
 
   public getClassForStatus(status: string) {
     if (status === 'Confirmed') {
