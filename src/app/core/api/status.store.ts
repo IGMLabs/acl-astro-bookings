1dea55ed8ca2237f211b3dd89656853fe9df9aae
Alejandro <alixxantada@gmail.com>
2022-06-10 14:47:15 +0200

    status api ok

---
+++ src/app/core/api/status.store.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ApiStatus } from './api-status.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StatusStore {
+  private initialState: ApiStatus = {
+    isWorking: false,
+    errorMessage: '',
+  };
+
+  // El subject es el productor y emisor
+  private state$: BehaviorSubject<ApiStatus> = new BehaviorSubject(
+    this.initialState
+    );
+
+
+  public setState(newState: ApiStatus) {
+    this.state$.next(this.initialState);
+  }
+
+  
+  public getState$(){
+    return this.state$.asObservable();
+  }
+}
+
+
+// class Productora {
+//   private statusStore = new StatusStore();
+
+//   public hacerCosas() {
+//     this.statusStore.setState({ isWorking: true, errorMessage: '' })
+//   }
+
+//   public descansar() {
+//     this.statusStore.setState({ isWorking: false, errorMessage: '' })
+//   }
+// }
+
+// class Consumidora {
+//   private statusStore = new StatusStore();
+
+//   public apiStatus!: ApiStatus;
+
+//   private pintarCambios() {
+//     this.statusStore
+//       .getState$()
+//       .subscribe( (currentState) => (this.apiStatus = currentState));
+
+//   }
+// }
+
+
+
+
