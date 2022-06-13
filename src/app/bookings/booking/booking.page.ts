94583af5d757a3b827f1977c1d65f1015fcde30e
Alejandro <alixxantada@gmail.com>
2022-06-13 11:01:55 +0200

    view booking ok

--- src/app/bookings/booking/booking.page.ts
+++ src/app/bookings/booking/booking.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Booking } from '../../core/api/booking.interface';
+import { BookingsApi } from '../../core/api/bookings.api';
 
 @Component({
   selector: 'app-booking',
@@ -7,7 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingPage implements OnInit {
 
-  constructor() { }
+  public bookingId: string;
+  public booking$: Observable<Booking>;
+
+  constructor( route:ActivatedRoute, bookingsApi: BookingsApi ) {
+    this.bookingId = route.snapshot.paramMap.get('id') || '';
+    this.booking$ = bookingsApi.getById$(this.bookingId);
+
+   }
 
   ngOnInit(): void {
   }
--- src/app/bookings/booking/booking.view.ts
+++ src/app/bookings/booking/booking.view.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Booking } from '../../core/api/booking.interface';
 
 @Component({
-  selector: 'app-booking',
+  selector: 'app-booking-view',
   templateUrl: './booking.view.html',
   styleUrls: ['./booking.view.css']
 })
 export class BookingView implements OnInit {
+
+  @Input() public bookingId!: string;
+  @Input() public booking?: Booking;
 
   constructor() { }
 
