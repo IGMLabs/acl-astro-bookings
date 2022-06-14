f335bc6914f5874c92abe8a4ba7c0a1c4dab1e9e
Alejandro <alixxantada@gmail.com>
2022-06-14 11:06:04 +0200

    bug arreglado form

--- src/app/shared/controls/email/email.control.ts
+++ src/app/shared/controls/email/email.control.ts
@@ -23,19 +23,19 @@ export class EmailControl extends FormBase implements OnInit, ControlValueAccess
   constructor(fms: FormMessagesService) {
 
     super(fms);
-    super.form = new FormGroup({
+    this.form = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
     })
    }
 
 
   writeValue(value: any): void {
-    super.form.setValue({ email: value }, { emitEvent: false });
+    this.form.setValue({ email: value }, { emitEvent: false });
   }
 
 
   registerOnChange(changeCallBack: any): void {
-    super.form.valueChanges.subscribe(changeCallBack);
+    this.form.valueChanges.subscribe(changeCallBack);
   }
 
 
